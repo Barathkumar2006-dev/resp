6a1e454d2f74df480d4735a303141ea29c0ead96
Barathkumar2006-dev <mabarathkumar@gmail.com>
2024-08-07 07:23:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+/// main-content is start 
+
+const header = document.querySelector("head");
+
+const main = document.querySelector(".main-content");
+
+const leftContent =  document.getElementsByClassName("left-content");
+
+console.log(leftContent);
+
+
